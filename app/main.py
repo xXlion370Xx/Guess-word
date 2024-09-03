f8b2d79df1eaec12be23b0ef29e1c76dafb6ffeb
Karen Vargas <kalivargas.11@gmail.com>
2024-09-02 23:48:48 -0500

    Investigación uso de la clase WebSocket

--- app/main.py
+++ app/main.py
@@ -1,6 +1,34 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket
+from typing import List
 
 app = FastAPI()
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: List[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    async def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+manager = ConnectionManager()
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await manager.broadcast(f"Mensaje de usuario: {data}")
+    except Exception as e:
+        await manager.disconnect(websocket)
 
 
 @app.get("/")

222d41162203ed70a8fd5b77628d21f0dedbb2b4
Karen Vargas <kalivargas.11@gmail.com>
2024-09-19 20:33:21 -0500

    Method random word :)

--- server/app/services/CodeGeneratorManager.py
+++ server/app/services/CodeGeneratorManager.py
@@ -1,5 +1,7 @@
 import random
 import string
+import requests 
+
 class CodeGeneratorManager:
     def gen_random_letters(self) -> list:
         return random.choices(string.ascii_letters, k=8)
@@ -9,3 +11,11 @@ class CodeGeneratorManager:
     
     def gen_random_code(self) -> string:
         return f'{self.gen_random_number()}' + ''.join(self.gen_random_letters())
+    
+    def gen_random_word(self) ->string:
+        url = requests.get('https://random-words-api.vercel.app/word/spanish')
+        if url.status_code == 200:
+            palabra = url.json()
+            return (palabra[0]['word'], 200)
+        else:
+            return ("Ups, hubo un error al generar la palabra, intenta de nuevo :)", 400)
